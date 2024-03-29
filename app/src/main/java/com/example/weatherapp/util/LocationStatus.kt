acb9594be12587dec3a3705aa147eb7091ea9581
Youssef Waleed <ywsoliman@gmail.com>
2024-03-29 16:40:41 +0200

    Unused files

--- app/src/main/java/com/example/weatherapp/util/LocationStatus.kt
+++
@@ -1,7 +0,0 @@
-package com.example.weatherapp.util
-
-sealed class LocationStatus {
-    data object Granted : LocationStatus()
-    data object Denied : LocationStatus()
-    data object Asking : LocationStatus()
-}
